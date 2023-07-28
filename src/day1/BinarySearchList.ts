207ef94378d8d9340f4e163da188efc802b64ffc
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-07-28 22:59:55 +0800

    bs_list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,25 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
+const getMiddle = (low: number, high: number): number => {
+    return low + Math.floor((high - low) / 2);
+};
 
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+
+    do {
+        const mid = getMiddle(low, high);
+        const hay = haystack[mid];
+
+        if (hay === needle) {
+            return true;
+        }
+
+        if (haystack[mid] < needle) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    } while (low < high);
+
+    return false;
 }
