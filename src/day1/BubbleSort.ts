3d6df7e1fb9156d73d62d36dd159a1898c9e36c2
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-07-29 23:06:33 +0800

    bubble_sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,15 @@
-export default function bubble_sort(arr: number[]): void {
+const swap = (arr: any[], a: number, b: number) => {
+    const temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+};
 
+export default function bubble_sort(arr: number[]): void {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
 }
