48747f90dd907da9794c886f7a8acc48bbd191a5
Samuel Jensen <samuel.jensen@ogglobal.net>
2023-08-01 17:08:26 +0800

    Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,51 @@
+type Node<T> = {
+    val: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head: Node<T> | undefined;
+    private tail: Node<T> | undefined;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        this.length++;
 
-}
+        const node = {
+            val: item,
+        };
+
+        if (!this.tail) {
+            this.tail = this.head = node;
+        } else {
+            this.tail.next = node;
+            this.tail = this.tail.next;
+        }
+    }
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+
+        const temp = this.head;
+
+        if (!this.head.next) {
+            this.head = this.tail = undefined;
+        } else {
+            this.head = this.head.next;
+        }
+
+        temp.next = undefined;
+        return temp.val;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
