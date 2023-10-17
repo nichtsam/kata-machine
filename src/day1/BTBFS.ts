9bcba069512d4247fa2bf9b1224ef1238ae10952
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-10-17 18:32:07 +0800

    BTBFS

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,14 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
 
+    let node;
+    while (typeof (node = q.shift()) !== "undefined") {
+        if (node.value === needle) {
+            return true;
+        }
+        node.left && q.push(node.left);
+        node.right && q.push(node.right);
+    }
+
+    return false;
 }
